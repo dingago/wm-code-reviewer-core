835c70060e56015740873fd7fac0fcfca9fbfdb7
Xiaowei Wang <dingago@gmail.com>
2019-08-28 14:13:24 -0600

    Added rule ServiceAuditingRule

---
+++ src/hx/codeReviewer/lang/wm/rule/performance/ServiceAuditingRule.java
@@ -0,0 +1,37 @@
+package hx.codeReviewer.lang.wm.rule.performance;
+
+import hx.codeReviewer.lang.wm.ast.ASTFlowService;
+import hx.codeReviewer.lang.wm.ast.ASTJavaService;
+import hx.codeReviewer.lang.wm.ast.AbstractBaseService;
+import hx.codeReviewer.lang.wm.ast.AbstractNsService.AuditOption;
+import hx.codeReviewer.lang.wm.rule.AbstractWmRule;
+
+/**
+ * 
+ * @author Xiaowei Wang
+ * @version 1.0
+ * 
+ *          Makes sure the service auditing is set properly for better
+ *          performance.
+ */
+public class ServiceAuditingRule extends AbstractWmRule {
+
+	@Override
+	public Object visit(ASTJavaService node, Object data) {
+		return visit((AbstractBaseService) node, data);
+	}
+
+	@Override
+	public Object visit(ASTFlowService node, Object data) {
+		return visit((AbstractBaseService) node, data);
+	}
+
+	private Object visit(AbstractBaseService node, Object data) {
+		if (node.getAudit() == AuditOption.ALWAYS) {
+			addViolation(data, node, new String[] { node.getAudit().toString(),
+					node.getNsName() });
+		}
+		return null;
+	}
+
+}
