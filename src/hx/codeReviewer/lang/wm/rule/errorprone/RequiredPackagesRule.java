9358a4e96150b2a5e68073c1b9354c56bb6784a0
Xiaowei Wang <dingago@gmail.com>
2019-08-28 23:05:08 -0600

    1. Support to check runtime package version in RuntimeUtil 2. Add rule RequiredPackagesRule

---
+++ src/hx/codeReviewer/lang/wm/rule/errorprone/RequiredPackagesRule.java
@@ -0,0 +1,39 @@
+package hx.codeReviewer.lang.wm.rule.errorprone;
+
+import java.util.Iterator;
+
+import com.wm.app.b2b.server.Manifest;
+import com.wm.app.b2b.server.Manifest.Requires;
+import com.wm.app.b2b.server.PackageManager;
+
+import hx.codeReviewer.lang.wm.ast.ASTPackage;
+import hx.codeReviewer.lang.wm.rule.AbstractWmRule;
+import hx.codeReviewer.util.RuntimeUtil;
+
+/**
+ * 
+ * @author Xiaowei Wang
+ * @version 1.0
+ * 
+ *          Makes sure the depended packages exists and match required version.
+ */
+public class RequiredPackagesRule extends AbstractWmRule {
+
+	public Object visit(ASTPackage node, Object data) {
+		Manifest manifest = node.getManifest();
+		Iterator<Requires> iter = manifest.getRequires().iterator();
+		while (iter.hasNext()){
+			Requires requires = iter.next();
+			String packageName = requires.getPackage();
+			String requiredVersion = requires.getVersion();
+			String actualVersion = RuntimeUtil.getPackageVersion(packageName);
+			if (actualVersion == null){
+				addViolation(data, node, packageName);
+			}else if (!PackageManager.checkDependencyVersion(requiredVersion, actualVersion)){
+				addViolation(data, node, packageName);
+			}
+		}
+		return null;
+	}
+
+}
--- src/hx/codeReviewer/util/RuntimeUtil.java
+++ src/hx/codeReviewer/util/RuntimeUtil.java
@@ -2,17 +2,56 @@ package hx.codeReviewer.util;
 
 import com.wm.app.b2b.server.ns.Namespace;
 import com.wm.lang.ns.NSNode;
+import com.wm.app.b2b.server.Package;
 
 import hx.codeReviewer.lang.wm.ast.AbstractNsNode;
 import hx.codeReviewer.lang.wm.ast.AbstractNsNode.NodeType;
 
+/**
+ * 
+ * @author Xiaowei Wang
+ * @version 1.1
+ * 
+ *          This class helps to retrieve dynamic information on runtime.
+ *
+ */
 public class RuntimeUtil {
+	/**
+	 * @author Xiaowei Wang
+	 * @since 1.0
+	 * @param nodeName
+	 *            The name of node to check.
+	 * @return The type of node.
+	 * 
+	 *         This method checks if a specific node exists, if so returns the
+	 *         type of node, otherwise returns NodeType.NONE.
+	 */
 	public static NodeType checkNodeExistence(String nodeName) {
 		NSNode nsNode = Namespace.current().getNode(nodeName);
 		if (nsNode == null) {
 			return NodeType.NONE;
 		} else {
 			return AbstractNsNode.getNodeType(nsNode.getValues());
+		}
+	}
+
+	/**
+	 * @author Xiaowei Wang
+	 * @since 1.1
+	 * @param packageName
+	 *            The name of package to check.
+	 * @return The version of package.
+	 * 
+	 *         This method checks if a specific package exists, if so returns
+	 *         the version of package, otherwise returns null.
+	 */
+	public static String getPackageVersion(String packageName) {
+		Package _package = (Package) Namespace.current()
+				.getPackage(packageName);
+		if (_package == null) {
+			return null;
+		} else {
+			return _package.getVersion();
 		}
 	}
 }
