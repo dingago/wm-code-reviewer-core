2980323a14be0ba74678db63b85653875874d8a3
Xiaowei Wang <dingago@gmail.com>
2019-08-28 23:16:54 -0600

    Mix incorrect description.

--- src/hx/codeReviewer/lang/wm/rule/errorprone/RequiredPackagesRule.java
+++ src/hx/codeReviewer/lang/wm/rule/errorprone/RequiredPackagesRule.java
@@ -15,21 +15,22 @@ import hx.codeReviewer.util.RuntimeUtil;
  * @author Xiaowei Wang
  * @version 1.0
  * 
- *          Makes sure the depended packages exists and match required version.
+ *          Makes sure the required packages exists and match required version.
  */
 public class RequiredPackagesRule extends AbstractWmRule {
 
 	public Object visit(ASTPackage node, Object data) {
 		Manifest manifest = node.getManifest();
 		Iterator<Requires> iter = manifest.getRequires().iterator();
-		while (iter.hasNext()){
+		while (iter.hasNext()) {
 			Requires requires = iter.next();
 			String packageName = requires.getPackage();
 			String requiredVersion = requires.getVersion();
 			String actualVersion = RuntimeUtil.getPackageVersion(packageName);
-			if (actualVersion == null){
+			if (actualVersion == null) {
 				addViolation(data, node, packageName);
-			}else if (!PackageManager.checkDependencyVersion(requiredVersion, actualVersion)){
+			} else if (!PackageManager.checkDependencyVersion(requiredVersion,
+					actualVersion)) {
 				addViolation(data, node, packageName);
 			}
 		}
