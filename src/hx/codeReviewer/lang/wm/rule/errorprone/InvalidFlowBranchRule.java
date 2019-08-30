d6490dfc2c42c11927b572faafef5ea56ac94428
Xiaowei Wang <dingago@gmail.com>
2019-08-30 10:16:22 -0600

    Support new rule InvalidFlowBranchRule

---
+++ src/hx/codeReviewer/lang/wm/rule/errorprone/InvalidFlowBranchRule.java
@@ -0,0 +1,32 @@
+package hx.codeReviewer.lang.wm.rule.errorprone;
+
+import hx.codeReviewer.lang.wm.ast.ASTFlowBranch;
+import hx.codeReviewer.lang.wm.rule.AbstractWmRule;
+
+/**
+ * 
+ * @author Xiaowei Wang
+ * @version 1.0
+ * 
+ *          Makes sure switch-on is not set if evaluate-label set to true, or
+ *          switch-no is set to some value if evaluate-label set to false for
+ *          flow branch.
+ */
+public class InvalidFlowBranchRule extends AbstractWmRule {
+
+	public Object visit(ASTFlowBranch node, Object data) {
+
+		if (!node.isEvaluateLabels()
+				&& (node.getSwitchOn() == null || node.getSwitchOn().isEmpty())) {
+			addViolation(data, node,
+					new String[] { node.getNsName(), node.getPath() });
+		}
+
+		if (node.isEvaluateLabels() && node.getSwitchOn() != null
+				&& !node.getSwitchOn().isEmpty()) {
+			addViolation(data, node,
+					new String[] { node.getNsName(), node.getPath() });
+		}
+		return super.visit(node, data);
+	}
+}
